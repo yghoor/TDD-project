a1bed4acedd567e2457946301c548001ec77ba4f
Yahya Ghoor <ghooryahya@gmail.com>
2022-10-10 13:20:58 +0200

    Fix linter errors

--- lib/solver.rb
+++ lib/solver.rb
@@ -1,35 +1,31 @@
 class Solver
   def factorial(num)
-    if num == 0
-      return 1
-    elsif num.negative?
-      return 'Error: cannot apply factorial to a negative number'
-    elsif num.is_a?(Integer) == false
-      return 'Error: cannot apply factorial to a non-integer value'
-    end
+    return 1 if num.zero?
+    return 'Error: cannot apply factorial to a negative number' if num.negative?
+    return 'Error: cannot apply factorial to a non-integer value' if num.is_a?(Integer) == false
 
     result = 1
     num.times { |i| result *= num - i }
-    return result
+    result
   end
 
   def reverse(string)
-    str_array = string.split('')
+    str_array = string.chars
     reversed_array = []
     string.length.times { reversed_array << str_array.pop }
 
-    return reversed_array.join
+    reversed_array.join
   end
 
   def fizzbuzz(num)
-    if num % 3 == 0 && num % 5 == 0
-      return 'fizzbuzz'
-    elsif num % 3 == 0
-      return 'fizz'
-    elsif num % 5 == 0
-      return 'buzz'
+    if (num % 3).zero? && (num % 5).zero?
+      'fizzbuzz'
+    elsif (num % 3).zero?
+      'fizz'
+    elsif (num % 5).zero?
+      'buzz'
     else
-      return "#{num}"
+      num.to_s
     end
   end
 end
