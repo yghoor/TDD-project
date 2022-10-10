75896c46f6cb17ebb862ebc23817fa4d065070d5
Yahya Ghoor <ghooryahya@gmail.com>
2022-10-10 12:11:06 +0200

    Write code for 'factorial' method

--- lib/solver.rb
+++ lib/solver.rb
@@ -1,2 +1,15 @@
 class Solver
+  def factorial(num)
+    if num == 0
+      return 1
+    elsif num.negative?
+      return 'Error: cannot apply factorial to a negative number'
+    elsif num.is_a?(Integer) == false
+      return 'Error: cannot apply factorial to a non-integer value'
+    end
+
+    result = 1
+    num.times { |i| result *= num - i }
+    return result
+  end
 end
