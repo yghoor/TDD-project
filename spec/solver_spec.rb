240597323b9014fad00074be7224274a3a46ef0f
Yahya Ghoor <ghooryahya@gmail.com>
2022-10-10 12:56:45 +0200

    Write tests for 'fizzbuzz' method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -34,4 +34,22 @@ describe Solver do
       expect(@solver.reverse('apple')).to eq('elppa')
     end
   end
+
+  context '#fizzbuzz' do
+    it 'takes an integer as an argument and returns \'fizz\' if it is divisible by 3' do
+      expect(@solver.fizzbuzz(12)).to eq('fizz')
+    end
+
+    it 'takes an integer as an argument and returns \'buzz\' if it is divisible by 5' do
+      expect(@solver.fizzbuzz(20)).to eq('buzz')
+    end
+
+    it 'takes an integer as an argument and returns \'fizzbuzz\' if it is divisible by 3 and 5' do
+      expect(@solver.fizzbuzz(30)).to eq('fizzbuzz')
+    end
+
+    it 'takes an integer as an argument and returns it as a string if it is divisible by neither 3 nor 5' do
+      expect(@solver.fizzbuzz(19)).to eq('19')
+    end
+  end
 end
