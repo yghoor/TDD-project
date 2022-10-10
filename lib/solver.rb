6c8712bd2b8e8a0b5c00f24141a01289996b6eb6
Yahya Ghoor <ghooryahya@gmail.com>
2022-10-10 12:58:41 +0200

    Write code for 'fizzbuzz' method

--- lib/solver.rb
+++ lib/solver.rb
@@ -20,4 +20,16 @@ class Solver
 
     return reversed_array.join
   end
+
+  def fizzbuzz(num)
+    if num % 3 == 0 && num % 5 == 0
+      return 'fizzbuzz'
+    elsif num % 3 == 0
+      return 'fizz'
+    elsif num % 5 == 0
+      return 'buzz'
+    else
+      return "#{num}"
+    end
+  end
 end
