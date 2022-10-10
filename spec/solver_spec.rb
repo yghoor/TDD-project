41f3c5b9b8174d866875e3b6cc9b04d1417d942a
Yahya Ghoor <ghooryahya@gmail.com>
2022-10-10 13:01:06 +0200

    Edit test names for 'factorial' method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -16,15 +16,15 @@ describe Solver do
       expect(@solver.factorial(4)).to eq(24)
     end
 
-    it 'returns 1 if the argument is 0' do
+    it 'takes one number as an argument and returns 1 if it is 0' do
       expect(@solver.factorial(0)).to eq(1)
     end
 
-    it 'raises an exception if the argument is not a positive number' do
+    it 'takes one number as an argument and raises an exception if it is not a positive number' do
       expect(@solver.factorial(-3)).to eq('Error: cannot apply factorial to a negative number')
     end
 
-    it 'raises an exception if the argument is not an integer' do
+    it 'takes one number as an argument and raises an exception if it is not an integer' do
       expect(@solver.factorial(3.2)).to eq('Error: cannot apply factorial to a non-integer value')
     end
   end
